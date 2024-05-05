f66b6b171594816bea86e592c8da7c404e909354
Nidhi <nidhigoda99@gmail.com>
2024-05-05 04:03:52 -0400

    Changes Added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
        
     # path for dealer reviews view
     path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
